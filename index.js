eaea4e8bc0a9394953df8ba3e0322caf95e34143
eman_ramadan1 <er1337@fayoum.edu.eg>
2025-12-07 20:35:03 +0200

    added latest practicum changes

--- index.js
+++ index.js
@@ -1,71 +1,74 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
+const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv");
+const path = require("path");
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "pug");
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const CUSTOM_OBJECT_TYPE = "2-195536657"; 
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+const headers = {
+  Authorization: `Bearer ${process.env.HUBSPOT_ACCESS_TOKEN}`,
+  "Content-Type": "application/json",
+};
 
-// * Code for Route 1 goes here
+//  Homepage Route
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}?properties=name,age,type`,
+      { headers }
+    );
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+    res.render("homepage", {
+      title: "My Custom Object Table",
+      records: response.data.results,
+    });
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.send("Error loading homepage data.");
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
+// GET Form Route
+app.get("/update-cobj", (req, res) => {
+  res.render("updates", {
+    title: "Add Custom Object",
+  });
+});
+
+// POST Route
+app.post("/update-cobj", async (req, res) => {
+  const { name, age, type } = req.body;
+
+  try {
+    await axios.post(
+      `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}`,
+      {
         properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
+          name,
+          age,
+          type,
+        },
+      },
+      { headers }
+    );
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.send("Error creating record.");
+  }
 });
-*/
 
-
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
